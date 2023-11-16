3cdd230746e16fe21e94f40154940b3ddafc1aa4
Jeffrey Davis <j.davis081248@gmail.com>
2023-11-16 14:39:48 -0600

    Lab Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", () =>
+{
+  newTaskForm = document.getElementById("create-task-form")
+
+  newTaskForm.addEventListener("submit", (event) => 
+  {
+    event.preventDefault()
+
+    const newTaskDescription = document.getElementById("new-task-description")
+    const newTask = newTaskDescription.value
+    
+    const ListedTasks = document.getElementById("tasks")
+    const newListNode = document.createElement("li")
+    newListNode.textContent = newTask + " "
+    ListedTasks.appendChild(newListNode)
+
+    const deleteButton = document.createElement("button")
+    deleteButton.textContent = "X"
+    deleteButton.setAttribute("data-description", newTask)
+    newListNode.appendChild(deleteButton)
+
+    newTaskForm.reset()
+
+    deleteButton.addEventListener("click", (event) => 
+    {
+      const deleteItem = event.target.parentNode
+      deleteItem.remove()
+    })
+  })
 });
